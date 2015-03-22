fb3804e2ee4f271f74bdb3df51f1335e53c0380a
tdelev <tomce.delev@gmail.com>
2015-03-22 01:39:22 +0100

    Created docker presentation

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+/**
+ * Created by tdelev on 21.3.15.
+ */
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          hostname: '*',
+          base: '../',
+          keepalive: true
+        }
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  grunt.registerTask('default', ['']);
+
+};
