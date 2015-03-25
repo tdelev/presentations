051381da8a268a4148e0298226ef9e3d17058d14
tdelev <tomce.delev@gmail.com>
2015-03-25 13:27:04 +0100

    Finalized presentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,6 +17,6 @@ module.exports = function(grunt) {
   });
   grunt.loadNpmTasks('grunt-contrib-connect');
 
-  grunt.registerTask('default', ['']);
+  grunt.registerTask('default', ['connect']);
 
 };
